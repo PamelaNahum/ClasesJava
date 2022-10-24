0fd5989e30880f1522307bbbe43fc46cec8703d9
PamelaNahum <pamela.nahum14@gmail.com>
2022-10-24 11:23:21 -0300

    clase 24-10

---
+++ Comentarios.java
@@ -0,0 +1,51 @@
+public class Comentarios {
+    //Variable 
+        //tipos de variables en JavaScript -> boolean, number, string, array, object
+        // var nombre = 1;
+        //tipos de variables de Java -> boolean, int (enteros), double(16 dígitos despues de la coma) 
+        //y float(7 dígitos despues de la coma)
+        // String (cadena de texto, se ocupa ""), char(solo un caracter, se ocupa '')
+        //array y object
+        //int numero = 7;
+        //String palabra = "palabra";
+        // matriz -> [1,'a',"palabra",4]
+        // areglos -> [1,2,5,4] -> arreglo[1] == 2
+        // arreglo de arreglos -> [[1,3,2],[5,8,6],[7,9,8]]
+        // diccionario -> [{id:1, nombre:"Nicolas"}, {id:2, nombre:"Kiara"}] 
+        // [{dia:'lunes', compras:[2000,600,500]}, {dia:'viernes', compras:[2000,600,500]}]
+        //-> diccionario[0] -> {id:1, nombre:"Nicolas"} diccionario[1]->{id:2, nombre:"Kiara"}
+        //diccionario[0].nombre -> "Nicolas", diccionario[1].id->2
+        //------------------------------//
+        /* como definimos los valores de una declaracion de usuario?
+         * id -> podría ser alfa numérico, en caso de ser alfa numético seria String,
+         * en caso contrario seria int
+         * nombre -> String
+         * mail -> String
+         * password -> String
+         * edad -> Int
+         * genero -> char 
+         * 
+        */
+
+        //Declaracion de variables
+        /*
+         * int nombre = 15;
+         * String palabra = "este es un string";
+         * Double decimal = 1.2354;
+         * 
+         * 
+         * nombre = "hola"; 
+         */
+
+         //Parsear variables (cambiar de tipo)
+
+         /*
+          * ParseDouble()->pasar desde String a Double
+          * ParseInt()->pasar desde String a Int
+          * double altura = 1.5;
+          * int alt = (int) altura;
+          * String num = "12";
+          * int nuevoNum = Integer.parseInt(num);
+          */
+    
+}
--- Main.java
+++ Main.java
@@ -4,27 +4,22 @@ public class Main {
     public static void main (String [] args){
         //para mostrar en consola en JavaScript -> console.log()
         //para mostrar en consola en Java
-        System.out.print("este es un mensaje en consola");
+        System.out.print("este es un mensaje en consola ");
         //este imprime con salto de linea
-        System.out.println("este es un mensaje en consola");
+        System.out.println("Hola");
         //para pedile información al usuario de ocupa
         Console console = System.console();
-        console.readLine();
-        //Variable 
-        //tipos de variables en JavaScript -> boolean, number, string, array, object
-        // var nombre = 1;
-        //tipos de variables de Java -> boolean, int (enteros), double(16 dígitos despues de la coma) y float(7 dígitos despues de la coma)
-        // String (cadena de texto, se ocupa ""), char(solo un caracter, se ocupa '')
-        //array y object
-        //int numero = 7;
-        //String palabra = "palabra";
-        // matriz -> [1,'a',"palabra",4]
-        // areglos -> [1,2,5,4] -> arreglo[1] == 2
-        // arreglo de arreglos -> [[1,3,2],[5,8,6],[7,9,8]]
-        // diccionario -> [{id:1, nombre:"Nicolas"}, {id:2, nombre:"Kiara"}] 
-        // [{dia:'lunes', compras:[2000,600,500]}, {dia:'viernes', compras:[2000,600,500]}]
-        //-> diccionario[0] -> {id:1, nombre:"Nicolas"} diccionario[1]->{id:2, nombre:"Kiara"}
-        //diccionario[0].nombre -> "Nicolas", diccionario[1].id->2
+        String nombre = console.readLine("inserte nombre: ");
+        System.out.println("este es el nombre: "+nombre);
+        //vamos a pedir a usuario 2 números
+        String num = console.readLine("inserte número 1: ");
+        int nuevoNum = Integer.parseInt(num);
+        String num2 = console.readLine("inserte número 2: ");
+        int nuevoNum2 = Integer.parseInt(num2);
+        int suma = nuevoNum + nuevoNum2;
+        //int suma = Integer.parseInt(num) + Integer.parseInt(num2); -> tambien es correcto
+        System.out.println("el número es: " + suma);
+        
         
     }
 }
