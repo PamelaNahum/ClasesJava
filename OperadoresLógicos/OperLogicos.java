34cb2c952b09db5fd87ffb71b84a4c95088c7714
PamelaNahum <pamela.nahum14@gmail.com>
2022-10-24 14:32:56 -0300

    operadores logicos

---
+++ OperadoresLógicos/OperLogicos.java
@@ -0,0 +1,30 @@
+import java.io.Console;
+
+public class OperLogicos {
+    public static void main(String[] args) {
+        // operadores lógicos en javaScript
+        /*
+         * y -> &&
+         * o -> ||
+         * negación lógica -> !
+         */
+        //Operadores de comparación 
+        /*
+         * comparaciones de numeros
+         * 4>7 -> falso
+         * 5<10 -> verdadero
+         * 9==4 -> falso
+         */
+        Console console = System.console();
+
+        int numero = Integer.parseInt(console.readLine("Ingresa el número 1: "));
+        String numero2 = console.readLine("Ingresa el número 2: ");
+        int numeroInt2 = Integer.parseInt(numero2);
+
+        if(numero>numeroInt2){
+            System.out.println("el primer número es mayor");
+        }else{
+            System.out.println("el segundo número es mayor");
+        }
+    }
+}
