a44cc7e406edb39e817cda3a54050aeaa8df6967
PamelaNahum <pamela.nahum14@gmail.com>
2022-10-26 17:02:07 -0300

    funciones inicial

---
+++ Funciones/Funciones.java
@@ -0,0 +1,72 @@
+public class Funciones {
+    /*funciones en JavaScript
+     * function nombreFuncion (){}
+     * 
+     * static (el tipo valor que va a devolver) NombreFuncion (){}
+     * 
+     * 2 tipos de funciones en Java
+     * las funciones que retornan -> static int NombreFuncion(){}
+     * y 
+     * las funciones que NO retornan -> static void NombreFuncion(){}
+     */
+    //Ejemplos
+    //Funcion que retorna un valor 
+    /*
+     * static int Suma (){
+     * int resultado = 0;
+     * int x = 10;
+     * 
+     * resultado = 4+5;
+     * 
+     * if(x>resultado){
+     *  return 1;
+     * }else{
+     *  return 0;
+     * }
+     * }
+     * int resultadoGuardado = Suma();
+     */
+    //Funcion que NO retorna
+    /*
+     * static void HolaMundo(){
+     * int resultado = 0;
+     * 
+     * resultado = 4+5;
+     * 
+     *  System.out.print(resultado);
+     * }
+     */
+    //Ejemplo función que retorna con parámetros
+    /* static String Suma (int num1, int num2){
+         int resultado = 0;
+         int x = 10;
+         
+         resultado = num1+num2;
+         
+         if(x>resultado){
+          return "x es mayor que la suma de num1 + num2";
+         }else{
+          return "x es menor que la suma de num1 + num2";
+         }
+         } */
+    //Ejemplo función que NO retorna con parámetros
+    /* static void Suma (int num1, int num2){
+         int resultado = 0;
+         int x = 10;
+         
+         resultado = num1+num2;
+         
+         if(x>resultado){
+          System.out.print("x es mayor que la suma de num1 + num2");
+         }else{
+          System.out.print("x es menor que la suma de num1 + num2");
+         }
+         } */
+
+
+
+    public static void main(String[] args) {
+        
+    }
+    
+}
